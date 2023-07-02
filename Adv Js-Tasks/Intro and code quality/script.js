43fac74fc1fac44ad42e6672d5d3996d693b050a
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-07-02 23:18:16 +0530

    adv js assg

---
+++ Adv Js-Tasks/Intro and code quality/script.js
@@ -0,0 +1,14 @@
+//Q1 Write a program to show an alert
+// alert("Hello world");
+
+
+//Q2 What will be the result for these expressions?
+// 5 > 4 // true
+// "apple" > "pineapple" //false
+// "2" > "12" //true
+// undefined == null // true
+// undefined === null //false
+// null == "\n0\n" //false
+// null === +"\n0\n" //false
+
+
